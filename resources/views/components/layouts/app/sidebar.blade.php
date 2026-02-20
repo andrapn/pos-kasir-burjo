214a1d0a5e20739a6eb645bd32d56160b975835d
andrapn <andraput988@gmail.com>
2026-02-21 01:10:12 +0700

    update theme

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-
-<head>
-    @include('partials.head')
-    <style>
-        [x-cloak] {
-            display: none !important;
-        }
-    </style>
-    @filamentStyles
-</head>
+<flux:header>
 
 <body class="min-h-screen bg-white dark:bg-zinc-800">
 <!-- Top Header -->
-<flux:header class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+<flux:header sticky class="z-40 border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
     <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left"/>
 
     <a wire:navigate href="{{ route('dashboard') }}"
